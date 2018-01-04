b88c92a2aacc249fdc61b307b100bc499f32f725
zhuyuan <41656955@qq.com>
2018-01-04 20:32:20 +0800

    version two

--- test.cpp
+++ test.cpp
@@ -1,7 +1,4 @@
 
-
-
-
 //Suppose we rent the cpu time in a strange old super computer and costing no more than 256 "clock" each time for example. 
 //So computing is enabled only after allowed and we need to divide our computing task into several one.
 //This is an example to simulate the IO operation that only enabled after select or (e)poll result.
@@ -14,6 +11,7 @@
 #include <stdlib.h>
 #include <stack>
 #include "cort_proto.h"
+
 std::stack<cort_proto*> scheduler_list;
 unsigned char the_clock = 1;
 
@@ -38,59 +36,75 @@ bool pop_execute_work(){
 }
 
 struct fibonacci_cort;
-struct fibonacci_cort : public cort_multi_awaitable{
-    
+struct fibonacci_cort : public cort_proto{
     fibonacci_cort *corts[2];
     int n;
     int result;
-    ~fibonacci_cort(){
-
-    }
-    CO_BEGIN(fibonacci_cort)
-        
-        if(++the_clock == 0){   //Oh you are not enabled work now.
-            push_work(this);
-            CO_AWAIT_AGAIN();
-        }
     
-        if(n < 2){
-            result = n;
-            //When the coroutine ended you can use data0 to send_back some information.
-            //Now we use it to tell the parent coroutine: it is finished without any further sub-coroutine waiting..
-            data0.result_ptr = 0;   
-            CO_RETURN();
-        }
-        corts[0] = new fibonacci_cort();
-        corts[0]->n = n - 1;
-        corts[1] = new fibonacci_cort();
-        corts[1]->n = n - 2;
+    //You can define anything.
+    ~fibonacci_cort(){
+    }
+    fibonacci_cort(int input): n(input){
+    }
 
-        //They may cost much time so we should wait their result.
+    CO_DECL(fibonacci_cort)
 
-        //CO_AWAIT_ALL(corts[0], corts[1]); //You can place no more than ten corts to await.
-        CO_AWAIT_RANGE(corts, corts+2);     //Or using forward iterator of coroutine pointer for variate count.
+    cort_proto* start(){
+        CO_BEGIN 
+            if(++the_clock == 0){   //Oh you are not enabled to work now.
+                push_work(this);
+                CO_AGAIN;
+            }
         
-        if(++the_clock == 0){   //Oh you are not enabled work now.
-            push_work(this);
-            CO_AWAIT_AGAIN();
-        }
-        result = corts[0]->result + corts[1]->result;
-        delete corts[0];
-        delete corts[1];
-    CO_END(fibonacci_cort)
+            if(n < 2){
+                result = n;
+                CO_RETURN;
+            }
+            corts[0] = new fibonacci_cort(n-1);
+            corts[1] = new fibonacci_cort(n-2);
+
+            //They may cost much time so we should wait their result.
+            
+            if(false){
+                //You can skip await using CO_GOTO_NEXT_STATE
+                CO_GOTO_NEXT_STATE;
+            }
+            //CO_AWAIT_ALL(corts[0], corts[1]); //You can place no more than ten corts for CO_AWAIT_ALL.
+            //CO_AWAIT_RANGE(corts, corts+2);   //Or using forward iterator of coroutine pointer for variate count.
+            //CO_AWAIT(corts[0]); 
+            //CO_AWAIT(corts[1]);     //Or await one bye one. They must be put in different lines!
+            
+            //CO_AWAIT_ALL_IF(true, corts[0], corts[1]);
+            //CO_AWAIT_ALL_IF(cond, cort) means:
+            //if(cond){CO_GOTO_NEXT_STATE;}
+            //CO_AWAIT_ALL(cort);
+            
+            //CO_AWAIT_RANGE_IF(true, corts, corts+2);
+            CO_AWAIT_IF(true, corts[0]); 
+            CO_AWAIT_IF(true, corts[1]);    
+            
+            if(++the_clock == 0){   //Oh you are not enabled to work now.
+                push_work(this);
+                CO_AGAIN;
+            }
+            result = corts[0]->result + corts[1]->result;
+            delete corts[0];
+            delete corts[1];
+        CO_END
+    }
 };
 
-
-int main(int argc, char* argv[]){
-    fibonacci_cort main_task;
+int main(int argc, char* argv[]){ 
     if(argc <= 1){
-        main_task.n = 18;
+        argc = 18;
     }
     else{
-        main_task.n = atoi(argv[1]);
+        argc = atoi(argv[1]);
     }
+    fibonacci_cort main_task(argc);
     main_task.start();
     while(pop_execute_work()){
+        //sleep(1);
     }
     printf("%d\n", main_task.result);
     return 0;
